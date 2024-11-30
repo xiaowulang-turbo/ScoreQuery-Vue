97835b8f009e370701eada8ed0910682cbe5f097
xiaowu <2728795232@qq.com>
2024-11-30 13:35:05 +0800

    refactor: rename auth to auths and update API endpoints

--- client/src/api/auths.js
+++ client/src/api/auths.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3010/auth'
+const baseUrl = 'http://localhost:3010/auths'
 
 /**
  * 登录用户
- * @param {Object} credentials - 用户凭据 { exam_id, password }
+ * @param {Object} credentials - 用户凭据 { userId, password }
  * @returns {Promise} - 返回用户信息和 token
  */
 export const login = credentials => {
@@ -12,8 +12,8 @@ export const login = credentials => {
 }
 
 /**
- * 注册用户
- * @param {Object} userData - 用户注册信息 { exam_id, password, name, gender, role }
+ * 注册用户s
+ * @param {Object} userData - 用户注册信息 { userId, password, name, gender, role }
  * @returns {Promise} - 返回成功或失败的响应
  */
 export const register = userData => {
@@ -22,9 +22,9 @@ export const register = userData => {
 
 /**
  * 查询用户成绩
- * @param {String} exam_id - 用户考生号
+ * @param {String} userId - 用户考生号
  * @returns {Promise} - 返回用户成绩
  */
-export const queryGrade = exam_id => {
-  return axios.post(`${baseUrl}/query`, { exam_id })
+export const queryScore = userId => {
+  return axios.post(`${baseUrl}/query`, { userId })
 }
